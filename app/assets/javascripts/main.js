2c702e9918df53515247e8460c5968d4fff7f15b
Ethan <evenitz@yahoo.com>
2012-07-27 17:45:41 -0400

    added some javascript

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -22,4 +22,27 @@ $(function() {
 
   $('#main_container').css("height", Math.max($('#tab_content_text').height() + 180, $('#tabs').find('ul').height() + 60, 600));
 
+  $('.remove_feat').click(function(event) {
+    event.preventDefault();
+    $('#feed_content').toggle();
+    if ($('#feed_content').is(":visible")) {
+      $(".remove_feat").text("Remove Featured");
+    } else {
+      $(".remove_feat").text("Add Featured");
+    }
+  });
+
+  $('.add_brown').click(function(event) {
+    event.preventDefault();
+    $('.featured').toggleClass('featured_brown'); 
+    $('.pagination').toggleClass('pagination_brown');
+    $('.slides_container').toggleClass('no_shadow');
+    if ($('.featured_brown').is(":visible")) {
+      $('.add_brown').text("Remove Brown");
+    } else {
+      $('.add_brown').text("Add Brown");
+    }
+  });
+
+  
 });
